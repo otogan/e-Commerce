41801525fb12d14e13f6aa45a877038a632ad31f
otogan <onurtogan10@gmail.com>
2020-06-25 19:49:58 -0500

    #14 update register controller name to UserController

--- src/main/java/com/ecommerce/web/controller/UserController.java
+++ src/main/java/com/ecommerce/web/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Profile {
+public class UserController {
 
     @GetMapping("/profile")
     public String profile(Model model) {
