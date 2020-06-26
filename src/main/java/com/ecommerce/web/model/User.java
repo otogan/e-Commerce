d25a0569c1fb44bae6742906ea397717b536efb9
otogan <onurtogan10@gmail.com>
2020-06-25 21:34:47 -0500

    #19 create user model

---
+++ src/main/java/com/ecommerce/web/model/User.java
@@ -0,0 +1,78 @@
+package com.ecommerce.web.model;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateCreated;
+    private String role;
+
+    public User() {
+    }
+
+    public User(long id, String firstName, String lastName, LocalDate dateCreated, String role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateCreated = dateCreated;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
