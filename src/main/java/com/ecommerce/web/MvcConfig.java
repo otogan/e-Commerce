c1440481fa96928204060de7bdf8cb04eeb1eff1
otogan <onurtogan10@gmail.com>
2020-06-27 22:52:08 -0500

    last state

--- src/main/java/com/ecommerce/web/MvcConfig.java
+++ src/main/java/com/ecommerce/web/MvcConfig.java
@@ -1,4 +1,17 @@
 package com.ecommerce.web;
 
-public class MvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/user/profile").setViewName("profile");
+//        registry.addViewController("/login").setViewName("login");
+    }
 }
